747bf603d292014a26eaffc5156d48741ae7ad14
Levi_Bee <279369954@qq.com>
2024-01-25 13:37:02 +0800

    feat: 增加标签选择筛选，增加标签管理

--- eladmin-web/src/views/studio/studio_work_image/index.vue
+++ eladmin-web/src/views/studio/studio_work_image/index.vue
@@ -2,11 +2,63 @@
   <div class="app-container">
     <!--工具栏-->
     <div class="head-container">
+      <div v-if="crud.props.searchToggle">
+        <!-- 搜索 -->
+        <label class="el-form-item-label">作品id</label>
+        <el-input v-model="query.workId" clearable placeholder="作品id" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">作品尺寸</label>
+        <el-select
+          v-model="query.workStatus"
+          clearable
+          size="small"
+          placeholder="类别"
+          class="filter-item"
+          style="width: 90px"
+          @change="crud.toQuery"
+        >
+          <el-option
+            v-for="item in dict.studio_work_image_size"
+            :key="item.id"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+        <rrOperation :crud="crud" />
+      </div>
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
       <crudOperation :permission="permission" />
       <!--表单组件-->
       <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
-        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px" />
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+          <el-form-item label="作品id" prop="workId">
+            <el-input v-model="form.workId" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="图片地址" prop="workImage">
+            <el-input v-model="form.workImage" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="作品尺寸" prop="workStatus">
+            <el-select v-model="form.workStatus" filterable placeholder="请选择">
+              <el-option
+                v-for="item in dict.studio_work_image_size"
+                :key="item.id"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="作品x坐标">
+            <el-input v-model="form.workOffsetX" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="作品y坐标">
+            <el-input v-model="form.workOffsetY" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="作品图片高度">
+            <el-input v-model="form.workImageHeight" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="作品图片宽度">
+            <el-input v-model="form.workImageWidth" style="width: 370px;" />
+          </el-form-item>
+        </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button type="text" @click="crud.cancelCU">取消</el-button>
           <el-button :loading="crud.status.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
@@ -32,6 +84,10 @@
             <el-tag type="success" effect="dark">{{ scope.row.workStatus }}</el-tag>
           </template>
         </el-table-column>
+        <el-table-column prop="workOffsetX" label="作品x坐标" />
+        <el-table-column prop="workOffsetY" label="作品y坐标" />
+        <el-table-column prop="workImageHeight" label="作品图片高度" />
+        <el-table-column prop="workImageWidth" label="作品图片宽度" />
         <el-table-column v-if="checkPer(['admin','studioWorkImage:edit','studioWorkImage:del'])" label="操作" width="150px" align="center">
           <template slot-scope="scope">
             <udOperation
@@ -55,12 +111,12 @@ import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
 
-const defaultForm = { id: null, workId: null, workImage: null, workStatus: null }
+const defaultForm = { id: null, workId: null, workImage: null, workStatus: null, workOffsetX: null, workOffsetY: null, workImageHeight: null, workImageWidth: null, createTime: null, updateTime: null }
 export default {
   name: 'StudioWorkImage',
-  // eslint-disable-next-line vue/no-unused-components
   components: { pagination, crudOperation, rrOperation, udOperation },
   mixins: [presenter(), header(), form(defaultForm), crud()],
+  dicts: ['studio_work_image_size'],
   cruds() {
     return CRUD({ title: '工作室作品图', url: 'api/studioWorkImage', idField: 'id', sort: 'id,desc', crudMethod: { ...crudStudioWorkImage }})
   },
@@ -72,24 +128,26 @@ export default {
         del: ['admin', 'studioWorkImage:del']
       },
       rules: {
+        workId: [
+          { required: true, message: '作品id不能为空', trigger: 'blur' }
+        ],
+        workImage: [
+          { required: true, message: '图片地址不能为空', trigger: 'blur' }
+        ],
+        workStatus: [
+          { required: true, message: '作品尺寸不能为空', trigger: 'blur' }
+        ]
       },
-      imageList: [] // 存储图片的数组
+      queryTypeOptions: [
+        { key: 'workId', display_name: '作品id' },
+        { key: 'workStatus', display_name: '作品尺寸' }
+      ]
     }
   },
   methods: {
     // 钩子：在获取表格数据之前执行，false 则代表不获取数据
     [CRUD.HOOK.beforeRefresh]() {
       return true
-    },
-    showBigImage(imageUrl) {
-      // 在这个方法中展示大图，比如使用el-dialog组件
-      // 可以根据需要自定义模态框的样式和内容
-      this.$alert(`<img src="${imageUrl}" style="max-width: 100%;">`, '预览', {
-        dangerouslyUseHTMLString: true,
-        showCancelButton: false,
-        showConfirmButton: false,
-        closeOnClickModal: true
-      })
     }
   }
 }
